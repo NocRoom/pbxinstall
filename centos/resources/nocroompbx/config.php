5b8da879355285d09c7cb034a3df1321ab462bd7
MrCoolLV <ivanjr@nocroom.com>
2018-12-04 10:36:49 -0800

    Update config.php

--- centos/resources/nocroompbx/config.php
+++ centos/resources/nocroompbx/config.php
@@ -4,13 +4,13 @@
 	$db_type = 'pgsql'; //sqlite, mysql, pgsql, others with a manually created PDO connection
 
 //sqlite: the db_name and db_path are automatically assigned however the values can be overidden by setting the values here.
-	//$db_name = 'fusionpbx.db'; //host name/ip address + '.db' is the default database filename
-	//$db_path = '/var/www/fusionpbx/secure'; //the path is determined by a php variable
+	//$db_name = 'nocroompbx.db'; //host name/ip address + '.db' is the default database filename
+	//$db_path = '/var/www/nocroompbx/secure'; //the path is determined by a php variable
 
 //pgsql: database connection information
 	$db_host = 'localhost'; //set the host only if the database is not local
 	$db_port = '5432';
-	$db_name = 'fusionpbx';
+	$db_name = 'nocroompbx';
 	$db_username = '{database_username}';
 	$db_password = '{database_password}';
 
