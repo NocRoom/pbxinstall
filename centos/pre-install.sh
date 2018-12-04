289aebe50f4a6ec3bdbd845f86adb0b2347c4da0
MrCoolLV <ivanjr@nocroom.com>
2018-12-04 15:56:57 -0800

    Update pre-install.sh

--- centos/pre-install.sh
+++ centos/pre-install.sh
@@ -4,7 +4,7 @@
 yum -y install git
 
 #get the install script
-cd /usr/src && git clone https://github.com/NocRoom/NocRoomPBX.git
+cd /usr/src && git clone https://github.com/NocRoom/pbxinstall.git
 
 #change the working directory
 cd /usr/src/NocRoomPBX/centos
